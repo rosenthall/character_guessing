bf66d488ff54bd2b09a7838493d37bf57c9047c6
Rosenthall <rosenthall@riseup.net>
2023-07-31 11:42:12 +0300

    declared basic functions

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -1,3 +1,11 @@
 mod model;
 
+// Если пользователь есть в актуальной дб - возвращает структуру User, в противном случае возвращает None
+pub async fn check_user(id : u8) -> Option<model::User> {
+    todo!()
+}
 
+// Функция добавляет пользователя в базу данных, в случае ошибки - возвращает Err()
+pub async fn try_add_user(user : model::User) -> Result<(), Err(e)> {
+    todo!();
+}
