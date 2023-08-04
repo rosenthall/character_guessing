6e4d23fe734a94203b0f933618805d741d80ec60
Rosenthall <rosenthall@riseup.net>
2023-08-04 19:03:53 +0300

    Создана базовая версия модуля для запросов к openai API

---
+++ openai/src/lib.rs
@@ -0,0 +1,55 @@
+use config::CONFIG;
+
+use log::*;
+
+use async_openai::{Client, config::OpenAIConfig};
+use async_openai::types::{ChatCompletionRequestMessageArgs, CreateChatCompletionRequestArgs, Role};
+
+
+pub async fn question(question : String) -> String {
+
+    let config = OpenAIConfig::new()
+        .with_api_key(CONFIG.clone().openai.openai_api_token);
+
+    // Получаем промпт для роли "ChatGPT" из конфигурации
+    let chatgpt_prompt = CONFIG.calendar.try_get_daily_prompt().unwrap();
+
+    // Получаем промпт для вопроса от пользователя (аргумент функции)
+    let user_prompt = question;
+    info!("Получен новый запрос от пользователя : {}", &user_prompt.clone());
+
+    // Создаем клиента для работы с OpenAI API
+    let client = Client::with_config(config);
+
+    // Формируем запрос на создание чат-подобной модели с указанными ролями и сообщениями
+    let request = CreateChatCompletionRequestArgs::default()
+        .max_tokens(192_u16)
+        .model("gpt-3.5-turbo")
+        .messages([
+            // Сообщение с ролью "System" для установки контекста ассистента
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::System)
+                .content("You are a historical character.You're trying to hide your name.")
+                .build().unwrap(),
+
+            // Сообщение с ролью "ChatGPT" и содержанием промпта для ChatGPT
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::Assistant) // Здесь ассистент играет роль ChatGPT
+                .content(chatgpt_prompt)
+                .build().unwrap(),
+
+            // Сообщение с ролью "User" и содержанием вопроса пользователя
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::User)
+                .content(&user_prompt)
+                .build().unwrap(),
+        ])
+        .build().unwrap();
+
+
+    let response = client.chat().create(request).await.unwrap();
+    info!("Ответ от openai api : {:#?}", response);
+
+
+    response.object
+}
