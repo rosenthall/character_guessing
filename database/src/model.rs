7a1b4d66084bf68e849b155fd9186ffc0009f9d7
armagg <rosenthall@riseup.net>
2023-08-15 20:18:37 +0300

    Added basic winner entry

--- database/src/model.rs
+++ database/src/model.rs
@@ -1,3 +1,4 @@
+// Нужно для хранения пользователей в течении дня.
 #[derive(Debug)]
 pub struct UserDbEntry {
     // Соответствует айди пользователя в телеграме
@@ -8,4 +9,13 @@ pub struct UserDbEntry {
     pub is_won: bool,
     // Количество вопросов
     pub questions_quantity: u8,
+}
+
+// Нужно для постоянного хранения победителей и их запаса вопросов к Chatgpt
+#[derive(Debug)]
+pub struct WinnerEntry {
+    // Айди в телеграме
+    pub id: u64,
+    // Количество оставшихся вопросов к chatgpt.
+    pub requests : u16
 }
